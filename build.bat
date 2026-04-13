3d9b57877e0413150bb7d38ea81d4ef82f5becae
BBDS\charlie.bishop <charlie.bishop@balfourbeatty.com>
2026-04-13 09:59:06 +0100

    More apps

--- build.bat
+++ build.bat
@@ -1,23 +1,20 @@
 @REM May have to highlight and run one by one if only does first one
 
-echo Building Deskbooker Theme...
 .\compiler\sass.bat scss/deskbooker.scss css/deskbooker-bootstrap.css
-echo Deskbooker done.
 
-echo Building HSES Theme...
 .\compiler\sass.bat scss/hses.scss css/hses-bootstrap.css
-echo HSES done.
 
-echo Building Iris Theme...
+.\compiler\sass.bat scss/property.scss css/property-bootstrap.css
+
 .\compiler\sass.bat scss/iris.scss css/iris-bootstrap.css
-echo Iris done.
 
-echo Building Portal Theme...
 .\compiler\sass.bat scss/portal.scss css/portal-bootstrap.css
-echo Portal done.
 
-echo Building Quality Theme...
+.\compiler\sass.bat scss/wims.scss css/wims-bootstrap.css
+
+.\compiler\sass.bat scss/r_and_d.scss css/r_and_d-bootstrap.css
+
 .\compiler\sass.bat scss/quality.scss css/quality-bootstrap.css
-echo Quality done.
+
 
 echo Build complete!
