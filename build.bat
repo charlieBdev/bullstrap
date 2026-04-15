7158f081db910064c582c3f9dff240072983ee91
charliebdev <charliebdev@hotmail.com>
2026-04-15 19:47:31 +0100

    Yap

--- build.bat
+++ build.bat
@@ -1,17 +1,17 @@
 @REM May have to highlight and run one by one if only does first one
 
-.\compiler\sass.bat scss/deskbooker.scss css/deskbooker-bootstrap.css
+.\compiler\sass.bat --quiet-deps --no-source-map scss/deskbooker.scss css/deskbooker-bootstrap.css
 
-.\compiler\sass.bat scss/hses.scss css/hses-bootstrap.css
+.\compiler\sass.bat --quiet-deps --no-source-map scss/hses.scss css/hses-bootstrap.css
 
-.\compiler\sass.bat scss/property.scss css/property-bootstrap.css
+.\compiler\sass.bat --quiet-deps --no-source-map scss/property.scss css/property-bootstrap.css
 
-.\compiler\sass.bat scss/iris.scss css/iris-bootstrap.css
+.\compiler\sass.bat --quiet-deps --no-source-map scss/iris.scss css/iris-bootstrap.css
 
-.\compiler\sass.bat scss/portal.scss css/portal-bootstrap.css
+.\compiler\sass.bat --quiet-deps --no-source-map scss/portal.scss css/portal-bootstrap.css
 
-.\compiler\sass.bat scss/wims.scss css/wims-bootstrap.css
+.\compiler\sass.bat --quiet-deps --no-source-map scss/wims.scss css/wims-bootstrap.css
 
-.\compiler\sass.bat scss/randd.scss css/randd-bootstrap.css
+.\compiler\sass.bat --quiet-deps --no-source-map scss/randd.scss css/randd-bootstrap.css
 
-.\compiler\sass.bat scss/quality.scss css/quality-bootstrap.css
+.\compiler\sass.bat --quiet-deps --no-source-map scss/quality.scss css/quality-bootstrap.css
