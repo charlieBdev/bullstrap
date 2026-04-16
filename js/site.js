cb467211ee98d752c8a2a46930619d372d486b73
charliebdev <charliebdev@hotmaill.com>
2026-04-16 23:33:16 +0100

    add cbtoast

--- js/site.js
+++ js/site.js
@@ -1,3 +1,5 @@
+import cbToast from 'https://rawcdn.githack.com/charlieBdev/cbToast/fc75514980cbd1382a0c6232a8539afa1d684db0/dist/cbToast.min.js';
+
 function setTooltips() {
     const tooltipTriggerList = document.querySelectorAll(
         '[data-bs-toggle="tooltip"]',
@@ -114,3 +116,63 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () 
         toggleTheme('system');
     }
 });
+
+// add event listener for toast button
+document.addEventListener("DOMContentLoaded", function () {
+    const toastBtn = document.getElementById("toastBtn");
+    if (toastBtn) {
+        const optionsArray = [
+            {},
+            {
+                title: "Info",
+                message: "This is an info toast notification.",
+                type: "info",
+                position: "top-right",
+                duration: 3000,
+                onClose: () => console.log("Info toast closed")
+            },
+            {
+                title: "Success",
+                message: "This is a success toast notification.",
+                type: "success",
+                position: 'bottom-right',
+                duration: 3000,
+                onClose: () => console.log("Success toast closed")
+            },
+            {
+                title: "Warning",
+                message: "This is a warning toast notification.",
+                type: "warning",
+                position: 'bottom-left',
+                duration: 3000,
+                onClose: () => console.log("Warning toast closed")
+            },
+            {
+                title: "Error",
+                message: "This is an error toast notification.",
+                type: "error",
+                position: 'top-left',
+                duration: 3000,
+                onClose: () => console.log("Error toast closed")
+            }
+        ];
+
+        toastBtn.addEventListener("click", function () {
+
+            const randomOptions = optionsArray[Math.floor(Math.random() * optionsArray.length)];
+
+            // Use the object directly
+            if (Object.keys(randomOptions).length !== 0) {
+                randomOptions.lightMode = Math.random() < 0.5;
+                randomOptions.countdown = Math.random() < 0.5;
+            }
+
+            new cbToast(randomOptions);
+        });
+    }
+});
+
+window.toggleTheme = toggleTheme;
+window.swapAppTheme = swapAppTheme;
+window.handleComponentSelect = handleComponentSelect;
+window.setTooltips = setTooltips;
